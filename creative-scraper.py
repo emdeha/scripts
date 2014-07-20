88205f7de0c50bcceca73a1df2c2c93e5dcb42ba
cpi-cecko <cpi.cecko@gmail.com>
2014-07-20 19:48:13 +0300

    gets the routines corresponding to a given person

--- creative-scraper.py
+++ creative-scraper.py
@@ -1,24 +1,26 @@
 from lxml import html
 import requests
 
-page = requests.get("https://podio.com/site/creative-routines")
-tree = html.fromstring(page.text)
 
-personInfo = '//div[@class="name"][<id>]/preceding-sibling::ul[@class="person"]'
-queryTemplate = '//li[contains(@class,"<attr>")]/@style'
+def doScrape() :
+    page = requests.get("https://podio.com/site/creative-routines")
+    tree = html.fromstring(page.text)
 
-names = tree.xpath('//div[@class="name"]/text()')
-namesLength = len(names)
+    personInfo = '//div[@class="name"][<id>]/preceding-sibling::ul[@class="person"]'
+    queryTemplate = '//li[contains(@class,"<attr>")]/@style'
 
-for idx in range(namesLength):
-    sleepStyles = tree.xpath(personInfo.replace("<id>",str(idx)) +
-                             queryTemplate.replace("<attr>","sleep"))
-    workStyles = tree.xpath(personInfo.replace("<id>",str(idx)) +
-                            queryTemplate.replace("<attr>","creative"))
-    leasureStyles = tree.xpath(personInfo.replace("<id>",str(idx)) +
-                               queryTemplate.replace("<attr>","food"))
-    exerciseStyles = tree.xpath(personInfo.replace("<id>",str(idx)) +
-                                queryTemplate.replace("<attr>","exercise"))
+    names = tree.xpath('//div[@class="name"]/text()')
+    namesLength = len(names)
 
-    print names[idx]
-    print sleepStyles
+    for idx in range(namesLength):
+        sleepStyles = tree.xpath(personInfo.replace("<id>",str(idx)) +
+                                 queryTemplate.replace("<attr>","sleep"))
+        workStyles = tree.xpath(personInfo.replace("<id>",str(idx)) +
+                                queryTemplate.replace("<attr>","creative"))
+        leasureStyles = tree.xpath(personInfo.replace("<id>",str(idx)) +
+                                   queryTemplate.replace("<attr>","food"))
+        exerciseStyles = tree.xpath(personInfo.replace("<id>",str(idx)) +
+                                    queryTemplate.replace("<attr>","exercise"))
+
+        print names[idx]
+        print sleepStyles
